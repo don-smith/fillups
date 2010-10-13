f3b233bdbed7d207836a7014581c2ba877b6bf65
Don Smith <locksmithdon@gmail.com>
2010-10-12 19:41:40 -0700

    added a missing require

--- router.rb
+++ router.rb
@@ -1,17 +1,6 @@
 require 'sinatra'
 require 'fillup'
 
-def create_fillup
-  Fillup.create!(:vehicle_name => 'Jeep',
-                 :location => 'Snohomish 76',
-                 :odometer => 12345,
-                 :gallons => 14.987,
-                 :date => Date.new,
-                 :rate => 3.012)
-end
-
 get '/' do
-  "love?"
-  # create_fillup
-  # Fillup.all().to_json
+  Fillup.all().to_json
 end
--- service.rb
+++ service.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.setup
 
 ENV['RACK_ENV'] ||= 'development'
+require File.join(File.dirname(__FILE__), 'config', 'config')
 
 require 'sinatra'
-require File.join(File.dirname(__FILE__), 'config', 'config')
+require 'router'
