3acbcffd0cbe53cde3136c2058b748f8ffdf0a9b
Don Smith <locksmithdon@gmail.com>
2010-10-14 00:33:16 -0700

    single or multiple fillups. added index.html

--- router.rb
+++ router.rb
@@ -1,5 +1,14 @@
 require 'fillup'
 
 get '/' do
+  "homepage"
+end
+
+get '/fillups' do
   Fillup.all().to_json
 end
+
+get '/fillups/:id' do
+  Fillup.all(:conditions => {:id => params[:id]}).to_json
+end
+
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -6,10 +6,21 @@ describe "the router" do
   def app
     Sinatra::Application
   end
+  
+  it 'can get to the main page' do
+    get '/'
+    last_response.should be_ok
+  end
+  
+  it 'can return all fillups' do
+    Factory(:fillup)
+    get '/fillups'
+    last_response.should be_ok
+  end
     
   it 'can return an existing fillup' do
-    Factory(:fillup)
-    get '/'
+    fillup = Factory(:fillup)
+    get "/fillups/#{fillup.id}"
     last_response.should be_ok
   end
     
